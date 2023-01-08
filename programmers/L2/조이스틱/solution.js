234e1e3a32a3d75d2f00dd80c6a763d00ea8a44f
yeongi <cbg5255@gmail.com>
2023-01-08 18:33:53 +0900

    hard : 조이스틱

--- programmers/L2/조이스틱/solution.js
+++ programmers/L2/조이스틱/solution.js
@@ -1,37 +1,45 @@
 function solution(name) {
   let cnt = 0;
+  const profit = [];
   let strArr = [...name];
 
   //알파벳 이동 횟수
-  strArr.forEach((char, index) => {
+  strArr.forEach((char) => {
     let ascChar = char.charCodeAt(0);
     if (ascChar >= 78) {
       let ascStr = 91;
-      while (ascChar != ascStr) {
-        ascStr -= 1;
-        cnt++;
-      }
+      cnt += ascStr - ascChar;
     }
-
     if (ascChar <= 77) {
       let ascStr = 65;
-      while (ascChar != ascStr) {
-        ascStr += 1;
-        cnt++;
-      }
+      cnt += ascChar - ascStr;
     }
   });
 
   //자리 이동 횟수
-  while (strArr.includes("A")) {
-    const arr = name.split("A");
-    console.log(arr);
-    strArr = arr[0].concat(arr[1]);
-  }
+  //즉, A가 시작되기 전의 문자열의 길이가 A가 연속되는 길이보다 짧을 때 뒤로 이동해야 한다.
+  const answer = strArr.reduce((acc, cur, index) => {
+    if (cur === "A") {
+      if (index - 1 < ACnt(name.slice(index))) {
+        profit.push(ACnt(name.slice(index)) - (index - 1));
+      }
+    }
+    return acc + 1;
+  }, 0);
 
-  cnt += strArr.length - 1;
-
-  return cnt;
+  return cnt + answer - Math.max(profit) - 1;
 }
 
-console.log(solution("JEROEN"));
+const ACnt = (string) => {
+  arr = string.split("");
+  let aIndex = arr.findIndex((char) => char === "A");
+  let count = 0;
+  while (string[aIndex] === "A") {
+    count += 1;
+    aIndex += 1;
+  }
+
+  return count;
+};
+
+console.log(solution("BBBAAAAABAAAAAAAAAAA"));

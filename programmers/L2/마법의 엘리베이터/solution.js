cdbed249ba8fd0574de44ea2d73aa3815d76c408
yeongi <cbg5255@gmail.com>
2023-01-11 19:35:20 +0900

    solve : 마법의 엘리베이터

--- programmers/L2/마법의 엘리베이터/solution.js
+++ programmers/L2/마법의 엘리베이터/solution.js
@@ -2,17 +2,27 @@ const solution = (storey) => {
   let div = storey;
   let sum = 0;
   while (div > 0) {
-    if (div % 10 > 5) {
-      sum += 10 - (div % 10);
-      if (div < 10) {
-        sum += 1;
+    if (div < 10) {
+      if (div <= 5) {
+        sum += div;
         break;
       }
-      div = Math.floor(div / 10);
-      div += 1;
+      if (div > 5) {
+        sum += 10 - div + 1;
+        break;
+      }
     }
 
-    if (div % 10 <= 5) {
+    if (div % 10 > 5) {
+      console.log(div);
+      sum += 10 - (div % 10);
+      div = Math.floor(div / 10);
+      div += 1;
+    } else if (div % 10 === 5) {
+      sum += div % 10;
+      div = Math.floor(div / 10);
+      if (div % 10 >= 5) div += 1;
+    } else {
       sum += div % 10;
       div = Math.floor(div / 10);
     }
@@ -21,10 +31,12 @@ const solution = (storey) => {
   return sum;
 };
 
-console.log(solution(95));
+console.log(solution(75));
 
 // | storey | result |
 // | ------ | ------ |
 // | 16     | 6      |
 // | 2554   | 16     |
 // | 95     | 6      |
+// |155     | 11     |
+// |154    | 10      |

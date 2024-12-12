fe62d9a8edfb0bb2de786ca9612e4c57fcc1d551
yeongi <cbg5255@gmail.com>
2024-12-12 16:31:06 +0900

    hard : 네트워크

---
+++ programmers/L3/네트워크.js
@@ -0,0 +1,22 @@
+function solution(n, computers) {
+  let networkCount = 0;
+  const visited = new Array(n).fill(false);
+
+  function dfs(node) {
+    visited[node] = true;
+    for (let i = 0; i < n; i++) {
+      if (computers[node][i] === 1 && !visited[i]) {
+        dfs(i);
+      }
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (!visited[i]) {
+      dfs(i);
+      networkCount++;
+    }
+  }
+
+  return networkCount;
+}
